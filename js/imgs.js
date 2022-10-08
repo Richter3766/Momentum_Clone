0352297212fa84ef84b79b9d70b292d249b7e348
Richter3766 <97567615+Richter3766@users.noreply.github.com>
2022-10-08 22:30:27 +0900

    .

--- js/imgs.js
+++ js/imgs.js
@@ -4,11 +4,11 @@ const imgName = []
 const imgNum = 11;
 
 for(let i = 0;i < imgNum;i++){
-    imgName.push("img/" + String(i).padStart(3, "0") + ".jpg");
+    imgName.push(String(i).padStart(3, "0") + ".jpg");
 }
 
 const imgIdx = Math.floor(Math.random()*imgName.length) ;
 const curImg = imgName[imgIdx];
 
-body.style.backgroundImage = `url("${curImg}")`;
+body.style.backgroundImage = `url("./img/${curImg}")`;
 console.log(imgIdx, curImg);
